16328a3a491fc1e05152a93438601394a001db31
Steven Velasquez-Chanci <sjv5461@psu.edu>
2023-10-16 17:23:39 -0400

    Add minimal working example of Eigen3 with CMake

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Eigen/Sparse>
+
+int main(int, char**){
+    const double pressureGradient = 0;
+    const double density = 0;
+
+    Eigen::VectorXd x;
+
+    return 0;
+}
