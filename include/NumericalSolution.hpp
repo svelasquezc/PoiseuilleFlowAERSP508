75f6b5089d56e45b3dcdc24f213bd23c788bc5b8
Steven Velasquez-Chanci <sjv5461@psu.edu>
2023-10-17 23:28:37 -0400

    Create folder include, add numerical sol as class

---
+++ include/NumericalSolution.hpp
@@ -0,0 +1,115 @@
+#ifndef NUMERICAL_SOLUTION_HPP
+#define NUMERICAL_SOLUTION_HPP
+
+#include <Eigen/Sparse>
+
+template<int N>
+class NumericalSolution
+{
+private:
+
+    /*
+    Aliases from Eigen types
+    */
+    using SparseMatrix = Eigen::SparseMatrix<double>;
+    using Triplet = Eigen::Triplet<double>;
+    using Vector = Eigen::VectorXd;
+    using Solver =  Eigen::BiCGSTAB<SparseMatrix, Eigen::IncompleteLUT<double, int> >; // Preconditioned solver
+
+
+    // Parameters 
+    const double timeDelta = 0.001;
+    mutable double deltaY = 0.01; // height/(N-1)
+
+    Vector gradientVector, velocityVector;
+    SparseMatrix lhsMatrix;
+    const int maxNonZeros = 3*N-4;
+
+    mutable double lambda = 0;
+    double time;
+
+    Solver solver;
+
+    Vector rhsExplicit (const Vector& velocityVector){
+        Vector rhs(N);
+        rhs.setZero();
+
+        for(int i=1; i<N-1;++i){
+            rhs[i]= lambda*velocityVector[i-1]/2 + (1-lambda)*velocityVector[i] + lambda*velocityVector[i+1]/2;
+        }
+
+        return rhs;
+    };
+
+public:
+    NumericalSolution(
+        double pressureGradient, 
+        double kinematicViscosity, 
+        double density, 
+        double height
+    ){
+
+        gradientVector.resize(N);
+        velocityVector.resize(N);
+        lhsMatrix.resize(N,N);
+        deltaY = height/(N-1);
+
+        lambda = kinematicViscosity*timeDelta/std::pow(deltaY, 2);
+
+        gradientVector.array() = -timeDelta*pressureGradient/density;
+        gradientVector[0] = 0; //Non-slip condition
+        gradientVector[N-1] = 0;//Non-slip condition
+
+        time = 0;
+        velocityVector.setZero(); //Initial Condition
+
+        /*
+        Matrix initialization: As the values of pressure gradient, viscosity, density, and the
+                               delta of time are constant over the whole simulation, the lambda
+                               parameter is constant, therefore the matrix remains unchanged, only 
+        */
+        {
+            auto nonZeros = std::vector<Triplet>();
+            nonZeros.reserve(maxNonZeros);
+
+            nonZeros.push_back({0,0,1});//Non-slip condition
+            
+
+            for (int i=1; i<N-1; ++i){
+                nonZeros.push_back({i,i-1,-lambda/2});
+                nonZeros.push_back({i,i,lambda+1});
+                nonZeros.push_back({i,i+1,-lambda/2});
+            }
+            nonZeros.push_back({N-1,N-1,1});//Non-slip condition
+            
+            lhsMatrix.reserve(maxNonZeros);
+            lhsMatrix.setFromTriplets(nonZeros.begin(), nonZeros.end());
+        }
+
+        //Initialize solver with matrix already defined
+        solver.preconditioner().setDroptol(0.00000001);
+        solver.compute(lhsMatrix);
+    }
+
+    void startSimulation(){
+        time = 0;
+        velocityVector.setZero();
+    }
+
+    void simulate(const double& goalTime){
+
+        while (time <= goalTime){
+            velocityVector = solver.solve(gradientVector + rhsExplicit(velocityVector));
+            time += timeDelta;
+        }
+    }
+
+    auto velocities() const{
+        std::vector<double> uvec(velocityVector.data(), velocityVector.data() + velocityVector.size());
+        return uvec;
+    }
+
+    ~NumericalSolution() = default;
+};
+
+#endif /* NUMERICAL_SOLUTION_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
-#include <Eigen/Sparse>
+
+#include <NumericalSolution.hpp>
+
 #include "matplotlibcpp.h"
 
 
@@ -12,84 +14,37 @@ namespace plt = matplotlibcpp;
 
 int main(int, char**){
 
-    using SparseMatrix = Eigen::SparseMatrix<double>;
-    using Triplet = Eigen::Triplet<double>;
-    using Vector = Eigen::VectorXd;
-    using Solver =  Eigen::BiCGSTAB<SparseMatrix, Eigen::IncompleteLUT<double, int> >;
-
-    constexpr int pressureGradient = -8;
-    constexpr unsigned int kinematicViscosity = 1;
-    constexpr unsigned int density = 1;
-
-    constexpr double timeDelta = 0.001;
-
-    constexpr double deltaY = 0.01;
-    constexpr auto N = 101;
-
-    constexpr auto lambda = kinematicViscosity*timeDelta/std::pow(deltaY, 2);
-
-
-    /* Initializing the whole vector as the first rhs term */
-    Vector gradientVector(N);
-    gradientVector.array() = -timeDelta*pressureGradient/density;
-    gradientVector[0] = 0;
-    gradientVector[N-1] = 0;
-
-    Vector velocityVector(N);
-    velocityVector.setZero();
-
-    SparseMatrix lhsMatrix;
-    {
-        constexpr auto maxNonZeros = 3*N-4;
-        auto nonZeros = std::vector<Triplet>();
-        nonZeros.reserve(maxNonZeros);
-
-        nonZeros.push_back({0,0,1});
-        
-
-        for (int i=1; i<N-1; ++i){
-            nonZeros.push_back({i,i-1,-lambda/2});
-            nonZeros.push_back({i,i,lambda+1});
-            nonZeros.push_back({i,i+1,-lambda/2});
-        }
-        nonZeros.push_back({N-1,N-1,1});
-        
-        lhsMatrix.resize(N,N);
-        lhsMatrix.reserve(maxNonZeros);
-        lhsMatrix.setFromTriplets(nonZeros.begin(), nonZeros.end());
-    }
-
-    auto rhsExplicit = [N, lambda](const Vector& velocityVector){
-        Vector rhs(N);
-        rhs.setZero();
-
-        for(int i=1; i<N-1;++i){
-            rhs[i]= lambda*velocityVector[i-1]/2 + (1-lambda)*velocityVector[i] + lambda*velocityVector[i+1]/2;
-        }
-
-        return rhs;
-    };
-
-    Solver solver;
-    solver.preconditioner().setDroptol(0.00000001);
-    solver.compute(lhsMatrix);
-
+    constexpr double pressureGradient = -8;
+    constexpr double kinematicViscosity = 1;
+    constexpr double density = 1;
+    constexpr double height = 1;
+    constexpr int N = 101;
+    constexpr double deltaY = height/(N-1);
     double time = 0;
-    double givenTime = 0.4;
-
-    while (time <= givenTime){
-
-        velocityVector = solver.solve(gradientVector + rhsExplicit(velocityVector));
-
-        time += timeDelta;
-    }
 
     auto heights = std::vector<double>(N);
-    std::vector<double> uvec(velocityVector.data(), velocityVector.data() + velocityVector.size());
-
     std::generate(heights.begin(), heights.end(), [n = 0, &deltaY]() mutable { return n++ * deltaY; });
 
-    plt::plot(heights, uvec);
+    NumericalSolution<N> numericalPoiseuille(pressureGradient, kinematicViscosity, density, height);
+
+    double time1 = 0.025, time2 = 0.05, time3=0.1, time4=0.4;
+    
+    numericalPoiseuille.simulate(time1);
+    auto numericalVelocitiesTime1 = numericalPoiseuille.velocities();
+    numericalPoiseuille.simulate(time2);
+    auto numericalVelocitiesTime2 = numericalPoiseuille.velocities();
+    numericalPoiseuille.simulate(time3);
+    auto numericalVelocitiesTime3 = numericalPoiseuille.velocities();
+    numericalPoiseuille.simulate(time4);
+    auto numericalVelocitiesTime4 = numericalPoiseuille.velocities();
+
+    plt::named_plot("time = 0.025 (Numerical Solution)", numericalVelocitiesTime1, heights);
+    plt::named_plot("time = 0.05 (Numerical Solution)", numericalVelocitiesTime2, heights);
+    plt::named_plot("time = 0.1 (Numerical Solution)", numericalVelocitiesTime3, heights);
+    plt::named_plot("time = 0.4 (Numerical Solution)", numericalVelocitiesTime4, heights);
+    
+    plt::title("Poiseuille flow Analytical vs Numerical Solution");
+    plt::legend();
     plt::show();
 
     return 0;
